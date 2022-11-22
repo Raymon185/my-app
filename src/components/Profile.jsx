68a371d795ec449093ba56dbb40d0dea5eacd1ca
Roman <raymon1852@gmail.com>
2022-11-22 21:23:38 +0300

    added new file

---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <img src='https://img.freepik.com/premium-photo/waterfall-landscape-plitvice-lakes-croatia_31965-5308.jpg?w=2000' />
+        <div>
+          <img src='https://cdn.britannica.com/91/181391-050-1DA18304/cat-toes-paw-number-paws-tiger-tabby.jpg' />
+          ava + description
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            <div>Post1</div>
+            <div>Post2</div>
+          </div>
+        </div>
+      </div>
+    )
+};
+
+export default Profile;
